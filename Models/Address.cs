c518da68f7946643e645dc50dff9f5457f440422
raphabraga <raphael-braga@hotmail.com>
2021-12-08 13:39:13 -0300

    created user and address models. corrected dependencies on program.cs and deleted nullable on .csproj

---
+++ Models/Address.cs
@@ -0,0 +1,16 @@
+namespace TrimaniaBackend.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+
+        public string Neighborhood { get; set; }
+
+        public string Number { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace TrimaniaBackend.Models
+{
+    public class User
+    {
+        private User() { }
+        public User(int id, string login, string name, string password, string cpf, string email, DateTime birthday, Address address)
+        {
+            this.Id = id;
+            this.Login = login;
+            this.Name = name;
+            this.Password = password;
+            this.Cpf = cpf;
+            this.Email = email;
+            this.Birthday = birthday;
+            this.Address = address;
+            CreationDate = DateTime.Now;
+        }
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+        public DateTime CreationDate { get; set; }
+        public Address Address { get; set; }
+    }
+}
