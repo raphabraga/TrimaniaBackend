fa417e0a82900bfa5c436cb183f0ffddd25672e0
raphabraga <raphael-braga@hotmail.com>
2021-12-09 02:42:40 -0300

    implemented modelbuild on appcontext. fixed docker-compose and launch settings for backend work properly along with mysql db

--- Backend/Data/ApplicationContext.cs
+++ Backend/Data/ApplicationContext.cs
@@ -6,11 +6,34 @@ namespace Backend.Data
 {
     public class ApplicationContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Address> Addresses { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseMySQL(Environment.GetEnvironmentVariable("DB_CString"));
         }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Address>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.State).IsRequired();
+                entity.Property(e => e.City).IsRequired();
+                entity.Property(e => e.Street).IsRequired();
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.HasIndex(e => e.Cpf).IsUnique();
+                entity.Property(e => e.Name).IsRequired();
+                entity.Property(e => e.Cpf).IsRequired();
+                entity.Property(e => e.Login).IsRequired();
+                entity.Property(e => e.Password).IsRequired();
+            });
+        }
     }
 }
