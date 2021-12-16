e572db1bb41b73b655ef010c12db16f5297f4793
raphabraga <raphael-braga@hotmail.com>
2021-12-16 12:59:32 -0300

    created enums for order model

---
+++ Backend/Models/OrderStatus.cs
@@ -0,0 +1,10 @@
+namespace Backend.Models
+{
+    enum OrderStatus
+    {
+        OPEN,
+        IN_PROGRESS,
+        CANCELLED,
+        COMPLETED
+    }
+}
---
+++ Backend/Models/PaymentMethod.cs
@@ -0,0 +1,9 @@
+namespace Backend.Models
+{
+    enum PaymentMethod
+    {
+        CREDIT_CARD,
+        BANK_SLIP,
+        IN_CASH
+    }
+}
