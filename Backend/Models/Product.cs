26c3bc3b1779be1c308c5beeb7562502052332cc
raphabraga <raphael-braga@hotmail.com>
2021-12-16 12:44:26 -0300

    created product model

---
+++ Backend/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace Backend.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public Product() { }
+    }
+}
