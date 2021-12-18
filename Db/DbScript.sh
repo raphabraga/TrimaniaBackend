05d250c92c063924b73d7347ca9faed2aa937257
raphabraga <raphael-braga@hotmail.com>
2021-12-18 08:58:07 -0300

    fixed entrypoint for populating db

--- Db/DbScript.sh
+++ Db/DbScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mysql -u root -proot < DbSetup.sql
+mysql -u root -proot < /Db/DbSetup.sql
 
 if [ $1 = -p ] || [ $1 = --populate ]; then
-    mysql -u trilogo -p1234 < DbPopulate.sql
+    mysql -u trilogo -p1234 < /Db/DbPopulate.sql
 fi
