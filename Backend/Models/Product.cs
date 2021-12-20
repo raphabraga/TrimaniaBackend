21dedf1b5453b4805d63cdff81717ddc85c3bb70
raphabraga <raphael-braga@hotmail.com>
2021-12-20 12:19:35 -0300

    adapted the previous implemented product to be the new ChartItem viewmodel and updated Product model according to the corrected definition

--- Backend/Models/Product.cs
+++ Backend/Models/Product.cs
@@ -3,8 +3,10 @@ namespace Backend.Models
     public class Product
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
         public decimal Price { get; set; }
-        public int Quantity { get; set; }
+        public int StockQuantity { get; set; }
         public Product() { }
     }
 }
---
+++ Backend/Models/ViewModels/ChartItem.cs
@@ -0,0 +1,9 @@
+namespace Backend.Models.ViewModels
+{
+    public class ChartItem
+    {
+        public Product Product { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
