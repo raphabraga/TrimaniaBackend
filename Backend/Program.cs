4314ca69cc1cbe024cd931f9633ae582c5eb9763
Raphael Braga <raphael-braga@hotmail.com>
2021-12-25 13:09:49 -0300

    added swashbuckle package. configured swagger at program.cs (#23)

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -55,11 +55,15 @@ builder.Services.AddScoped<ISalesReportService, SalesReportService>();
 builder.Services.AddScoped<ApplicationContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) { }
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 app.UseResponseCompression();
 app.UseHttpsRedirection();
 app.UseAuthentication();
